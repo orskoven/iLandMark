1878cdc2c1ce5626887bc174a752d9584d25b075
orskoven <simo26k2@stud.kea.dk>
2023-02-07 08:45:16 +0100

    Initial Commit

---
+++ iNotebook/iNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNotebookApp.swift
+//  iNotebook
+//
+//  Created by Simon Pedersen on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct iNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
